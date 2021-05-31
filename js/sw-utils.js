f1090106cfc451c35d1eefa1322a1e34aa032103
Dario Esquina <dario13esquina@gmail.com>
2021-05-31 11:09:11 -0500

    Primer Deploy GitHub-Pages PWA

---
+++ js/sw-utils.js
@@ -0,0 +1,20 @@
+
+
+//Guardar en el cache dinamico
+function actulizaCacheDinamico(dynamicCache, req, res) {
+    
+
+    if (res.ok) {
+        
+        return caches.open(dynamicCache).then(cache => {
+            
+            cache.put(req, res.clone());
+
+            return res.clone();
+        });
+
+    } else {
+        return res;
+    }
+
+}
